093c6ad97368a774780540d6d9dbf0b74b9786f4
moas98 <84452543+moas98@users.noreply.github.com>
2021-12-11 16:54:06 +0300

    Done

---
+++ Heart Disease Prediction/Heart Disease Prediction/Controllers/HeartDiseaseController.cs
@@ -0,0 +1,24 @@
+﻿using Heart_Disease_Prediction.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Heart_Disease_Prediction.Controllers
+{
+    public class HeartDiseaseController : Controller
+    {
+        [HttpGet]
+        public IActionResult Predict()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Predict(HeartData input)
+        {
+            var model = new MLModel();
+            model.Build();
+            var result = model.Consume(input);
+            ViewBag.HeartPrediction = result;
+            ViewBag.result = result;
+            return View();
+        }
+    }
+}
